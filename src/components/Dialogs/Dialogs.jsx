2e7dc6f0b6dab1368f824b71edd9a7ab5f2c81a5
Noway <nikav1999@yandex.ru>
2021-10-10 09:42:51 +0300

    nav and dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,29 @@ import React from "react";
 import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
-    return  <div>
-                Dialogs
+    return  <div className={s.dialogs}>
+                <div className={s.dialogs__items}>
+                    <div className={s.dialog + ' ' + s.active}>
+                        Dimych
+                    </div>
+                    <div className={s.dialog}>
+                        Nikita
+                    </div>
+                    <div className={s.dialog}>
+                        Valera
+                    </div>
+                    <div className={s.dialog}>
+                        Yura
+                    </div>
+                    <div className={s.dialog}>
+                        Vlad
+                    </div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hi</div>
+                    <div className={s.message}>How are you</div>
+                    <div className={s.message}>Yo</div>
+                </div>
             </div>
 }
 
